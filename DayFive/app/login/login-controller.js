f72649ea019127abd3aab5361dc6f6e4b37a1180
Rob Riggs <robr24@gmail.com>
2015-05-22 11:36:14 -0700

    more boilerplate for forms lessons

---
+++ DayFive/app/login/login-controller.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('scApp')
+        .controller('LoginController', Controller);
+
+    // Controller.$inject = ['dependencies'];
+
+    /* @ngInject */
+    function Controller() {
+        var vm = this;
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+        }
+    }
+})();
